4ba9ed8f3b9e34ecae19e160f35f308fc9651da6
Mark Miyashita <negativetwelve@gmail.com>
2019-05-09 03:09:32 -0700

    Create the increment/theme package and rename ui --> components

---
+++ themes/theme-basic/src/index.js
@@ -0,0 +1,15 @@
+// Libraries
+import Theme from '@increment/theme';
+
+// App
+import colors from './colors';
+import typography from './typography';
+
+// Styles
+import '../css/styles.css';
+import '../css/fonts/AvenirNext.css';
+
+export default new Theme({
+  colors,
+  typography,
+});
