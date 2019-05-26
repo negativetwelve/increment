33ee2c1d9d2c2da09bb1db3303760979e1eb747b
Mark Miyashita <negativetwelve@gmail.com>
2019-05-26 02:54:46 -0700

    Add back the styled TextInput, Touchable

--- packages/components/src/Styled/index.js
+++ packages/components/src/Styled/index.js
@@ -1,1 +1,14 @@
-export {default} from 'styled-x';
+/* eslint-disable max-len */
+
+// Libraries
+import Styled from 'styled-x';
+import {TextInput, TouchableOpacity} from 'react-native-web';
+
+// TODO(mark): There's a bug with the normal Touchable so we override it.
+Styled.Touchable = Styled(TouchableOpacity)`
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+`;
+
+Styled.TextInput = Styled(TextInput)``;
+
+export default Styled;
