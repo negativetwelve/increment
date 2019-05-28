87d69e2b779c825420d75a00e934d4fa6bf3e30e
Mark Miyashita <negativetwelve@gmail.com>
2019-05-27 18:05:47 -0700

    Add tests for the Modal component

---
+++ packages/components/src/Modal/__tests__/Modal.spec.dom.js
@@ -0,0 +1,10 @@
+// Libraries
+import React from 'react';
+import Modal from '../';
+
+/* eslint-disable no-undef */
+describe('Modal', () => {
+  context('with no props', () => {
+    itRenders(() => <Modal />);
+  });
+});
