3fef3de3fd4d5212816e90c4e73de2560bdba180
Mark Miyashita <negativetwelve@gmail.com>
2019-06-09 16:44:16 -0700

    Remove react-router from all dependencies

--- packages/app/src/components/Browser.js
+++ packages/app/src/components/Browser.js
@@ -1,15 +1,12 @@
 // Libraries
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
 
 // App
 import App from './App';
 
 const Browser = ({client, children}) => (
   <App client={client}>
-    <BrowserRouter>
-      {children}
-    </BrowserRouter>
+    {children}
   </App>
 );
 
--- packages/components/src/Link/index.native.js
+++
@@ -1,6 +0,0 @@
-// Libraries
-import React from 'react';
-
-const Link = ({children, ...props}) => children;
-
-export default Link;
